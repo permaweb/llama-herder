eb561f2c11d2dc948ec659c34d18c71cdfe06409
Sam Williams <sam@arweave.org>
2024-06-20 03:09:11 -0400

    Add Llama orchestration system, paying in wrapped AR.

--- router.lua
+++ router.lua
@@ -1,270 +1,102 @@
-local ao = require("ao")
-local json = require("json")
+WrappedAR = "xU9zFkq3X2ZQ6olwNVvr1vUWIjc3kXTWr7xKQD6dh10"
 
-WRAPPED_ARWEAVE_TOKEN_PROCESS = WRAPPED_ARWEAVE_TOKEN_PROCESS or "TODO: WrappedArweaveProcessId"
-
-LLAMA_BANKER_PROCESS = LLAMA_BANKER_PROCESS or "TODO: BankerProcessId"
-
-LLAMA_FED_CHAT_PROCESS = LLAMA_FED_CHAT_PROCESS or "TODO: ChatProcessId"
-
-LLM_WORKERS = LLM_WORKERS or {
-    ['4zQMuZlze_PoKcffdLTkXLv90_DusEENofq3Bg-hHQk'] = {
-        busyWithMessage = nil,
-        submittedTimestamp = nil,
-    },
-    ['FAKEWORKER2'] = {
-        busyWithMessage = nil,
-        submittedTimestamp = nil,
-    }
+Herd = {
+    -- List of open worker processes
+    "B0YVJ83M9KstjfZeCNoK0m7MnhzEVlSMz-GDcq5gPQw",
+    "YVjnwtjnY1vSXMiJobLNgkr5Ft_LTECFXD5BufDNAaA",
+    "J7zej-2rL0oJ8cvNR-RNzMicLgXUIRwLMxMvf1T9Q2E"
 }
+Busy = {}
+Queue = {}
 
-MESSAGES_TO_SEND = {
-    -- [oriingalMessageId] = {
-    --     originalMessageId = '1',
-    --     originalSender = 'wallet',
-    --     originalSenderName = 'my name', -- TODO
-    --     timestamp = 0,
-    --     content = "I want a grant for xyz",
-    -- }
-}
+SetPromptFee = 1
+FeePerToken = 1
 
-function clearExpiredLlamas(currentTime)
-    for llamaId, llama in pairs(LLM_WORKERS) do
-        if llama.busyWithMessage and currentTime - llama.submittedTimestamp >= 600 then
-            llama.busyWithMessage = nil
-            llama.submittedTimestamp = nil
-        end
+function CalculateFee(prompt, tokens)
+    local tokenCount = tokens
+    for _ in string.gmatch(prompt, "%w+") do
+       tokenCount = tokenCount + 1
     end
+
+    return SetPromptFee + (tokenCount * FeePerToken)
 end
 
-function isMessageProcessing(messageId)
-    for llamaId, llama in pairs(LLM_WORKERS) do
-        if llama.busyWithMessage == messageId then
-            return true
-        end
-    end
-    return false
-end
-
-function getHighestPriorityUnprocessedMessage()
-    table.sort(MESSAGES_TO_SEND, function(a, b) return a.timestamp < b.timestamp end)
-
-    for _, message in pairs(MESSAGES_TO_SEND) do
-        if not isMessageProcessing(message.originalMessageId) then
-            return message
-        end
-    end
-
-    return nil
-end
-
-function dispatchHighestPriorityMessage(currentTime)
-    clearExpiredLlamas(currentTime)
-
-    local highestPriorityMessage = getHighestPriorityUnprocessedMessage()
-    if highestPriorityMessage then
-        local messageId = highestPriorityMessage.originalMessageId
-        local llamaFound = false
-
-        for llamaId, llama in pairs(LLM_WORKERS) do
-            if not llama.busyWithMessage then
-                llama.busyWithMessage = messageId
-                llama.submittedTimestamp = currentTime
-                ao.send({
-                    Target = llamaId,
-                    Action = "Inference",
-                    ['Original-Sender'] = highestPriorityMessage.originalSender,
-                    ['Original-Message'] = highestPriorityMessage.originalMessageId,
-                    Data = highestPriorityMessage.content
-                })
-                llamaFound = true
-                break
-            end
+function DispatchWork()
+    for i in pairs(Herd) do
+        if #Queue == 0 then
+            return
         end
 
-        -- TODO: Check if this is correct? Shouldn't we have to delete it rather than reinserting it?
-        if not llamaFound then
-            table.insert(MESSAGES_TO_SEND, 1, highestPriorityMessage)
-        end
-    end
-end
+        local work = table.remove(Queue, 1)
+        print("Dispatching work. Worker: " .. Herd[i] .. "\nPrompt: " .. work.prompt)
+        ao.send({
+            Target = Herd[i],
+            Action = "Inference",
+            Tokens = work.tokens,
+            Data = work.prompt,
+        })
 
-function removeMessageAndResetLlama(messageId)
-    for _, message in pairs(MESSAGES_TO_SEND) do
-        if message.originalMessageId == messageId then
-            MESSAGES_TO_SEND[message.originalMessageId] = nil
-        end
-    end
-
-    for llamaId, llama in pairs(LLM_WORKERS) do
-        if llama.busyWithMessage == messageId then
-            llama.busyWithMessage = nil
-            llama.submittedTimestamp = nil
-            break
-        end
+        Busy[Herd[i]] = work.client
+        table.remove(Herd, i)
     end
 end
 
 Handlers.add(
-    "PetitionHandler",
-    Handlers.utils.hasMatchingTag("Action", "Petition"),
+    "Start-Inference",
     function(msg)
-        -- TODO: Check from Banker process
-        if (msg.From ~= LLAMA_BANKER_PROCESS) then
-            return print("Petition not from Banker")
+        return msg.Action == "Credit-Notice" and
+            msg.From == WrappedAR
+    end,
+    function(msg)
+        print("Inference request. Prompt: " .. msg["X-Prompt"])
+        local needed = CalculateFee(msg["X-Prompt"], tonumber(msg["X-Tokens"]))
+        if needed > tonumber(msg.Quantity) then
+            print("Start-Inference. Insufficient payment: " .. msg.Quantity)
+            ao.send({
+                Target = WrappedAR,
+                Recipient = msg.From,
+                Action = "Transfer",
+                Quantity = msg.Quantity,
+                ["X-Reason"] = "Insufficient payment",
+                ["X-Needed"] = tostring(needed)
+            })
+            return
         end
 
-        local originalMessageId = msg.Tags['Original-Message']
-        if (not originalMessageId) then
-            return print("No original message id found")
-        end
-        if (MESSAGES_TO_SEND[originalMessageId] ~= nil) then
-            return print("Message already exists")
-        end
+        table.insert(Queue, {
+            client = msg.Sender,
+            prompt = msg["X-Prompt"],
+            tokens = msg["X-Tokens"],
+            transferID = msg.Id
+        })
 
-        MESSAGES_TO_SEND[originalMessageId] = {
-            originalMessageId = originalMessageId,
-            originalSender = msg.Tags['Original-Sender'],
-            originalSenderName = msg.Tags['Original-Sender-Name'],
-            timestamp = msg.Timestamp,
-            content = msg.Data,
-        }
+        print("Inference request. Queue: " .. #Queue)
 
-        dispatchHighestPriorityMessage(msg.Timestamp)
+        DispatchWork()
     end
 )
-
-function isLlmWorker(processId)
-    return LLM_WORKERS[processId] ~= nil
-end
 
 Handlers.add(
     "InferenceResponseHandler",
     Handlers.utils.hasMatchingTag("Action", "Inference-Response"),
     function(msg)
-        if (not isLlmWorker(msg.From)) then
-            return print("Not a Llama Worker")
+        print("Inference-Response. From: " .. msg.From .. "\nData: " .. msg.Data)
+        print("Busy: " .. Busy[msg.From])
+        if not Busy[msg.From] then
+            print("Inference-Response not from worker.")
+            return
         end
-
-        local grade = tonumber(msg.Tags.Grade)
-        local reason = msg.Data
-        local originalMessageId = msg.Tags['Original-Message']
-        if (not originalMessageId) then
-            return print("No original message id found")
-        end
-        if (not MESSAGES_TO_SEND[originalMessageId]) then
-            return print("Message not found")
-        end
-
-        local originalSender = MESSAGES_TO_SEND[originalMessageId].originalSender
-        local originalSenderName = MESSAGES_TO_SEND[originalMessageId].originalSenderName
-        local useSender = originalSenderName or originalSender
-
-        removeMessageAndResetLlama(originalMessageId)
 
         ao.send({
-            Target = LLAMA_BANKER_PROCESS,
-            Tags = {
-                Action = "Grade-Petition",
-                Grade = tostring(grade),
-                ['Original-Message'] = originalMessageId,
-                ['Original-Sender'] = originalSender,
-                ['Original-Sender-Name'] = originalSenderName,
-            }
+            Target = Busy[msg.From],
+            Action = "Inference-Response",
+            Data = msg.Data,
         })
 
-        ao.send({
-            Target = LLAMA_FED_CHAT_PROCESS,
-            Tags = {
-                Action = 'ChatMessage',
-                ['Author-Name'] = 'Llama King',
-            },
-            Data = 'Dearest ' .. useSender .. ', here is my response to your petition: \r\n' .. reason,
-        })
+        Busy[msg.From] = nil
+        table.insert(Herd, msg.From)
 
-        dispatchHighestPriorityMessage(msg.Timestamp)
+        DispatchWork()
     end
 )
 
-Handlers.add(
-    "CronHandler",
-    Handlers.utils.hasMatchingTag("Action", "Cron-Tick"),
-    function(msg)
-        clearExpiredLlamas(msg.Timestamp)
-    end
-)
-
--- Schema
-
-
-function PetitionSchemaTags()
-    return [[
-{
-  "type": "object",
-  "required": [
-    "Action",
-    "Recipient",
-    "Quantity",
-    "X-Petition",
-    "X-Sender-Name"
-  ],
-  "properties": {
-    "Action": {
-      "type": "string",
-      "const": "Transfer"
-    },
-    "Recipient": {
-      "type": "string",
-      "const": "]] .. LLAMA_BANKER_PROCESS .. [["
-    },
-    "Quantity": {
-      "type": "number",
-      "minimum": 0.001,
-      "maximum": 0.1,
-      "title": "Wrapped $AR offering (0.001-0.1).",
-      "$comment": "1000000000000"
-    },
-    "X-Petition": {
-      "type": "string",
-      "minLength": 2,
-      "maxLength": 100,
-      "title": "Your written plea for $LLAMA"
-    },
-    "X-Sender-Name": {
-      "type": "string",
-      "minLength": 2,
-      "maxLength": 20,
-      "title": "Signed with your name"
-    }
-  }
-}
-]]
-end
-
-function SchemaExternal()
-    return {
-        Petition = {
-            Target = WRAPPED_ARWEAVE_TOKEN_PROCESS, -- Can be nil? In that case it must be supplied externally
-            Title = "Petition the Llama King",
-            Description =
-            "Offer some wrapped $AR tokens for a chance to earn $LLAMA coin. Make sure you have enough wrapped $AR in your ao wallet, or the Llama King will ignore you!",
-            Schema = {
-                Tags = json.decode(PetitionSchemaTags()),
-                -- Data
-                -- Result?
-            },
-        },
-    }
-end
-
-Handlers.add(
-    'SchemaExternal',
-    Handlers.utils.hasMatchingTag('Read', 'SchemaExternal'),
-    function(msg)
-        print('SchemaExternal')
-        Send({ Target = msg.From, Tags = { Type = 'SchemaExternal' }, Data = json.encode(SchemaExternal()) })
-    end
-)
-
--- PROFILE = "SNy4m-DrqxWl01YqGM4sxI8qCni-58re8uuJLvZPypY"
--- Send({ Target = PROFILE, Action = "Create-Profile", Data = '{"UserName":"Llama King","DateCreated":1718652121082,"DateUpdated":1718652121083,"ProfileImage":"","CoverImage":"","Description":"","DisplayName":"Llama King"}' })
--- worker.lua
+++ worker.lua
@@ -1,23 +1,6 @@
--- Module: 1PdCJiXhNafpJbvC-sjxWTeNzbf9Q_RfUNs84GYoPm0
-
 ModelID = "ISrbGzQot05rs_HKC08O_SmkipYQnqgB1yC3mjZZeEo"
+RouterID = "wh5vB2IbqmIBUqgodOaTvByNFDPr73gbUq1bVOUtCrw"
 Llama = Llama or nil
-
-DefaultMaxResponse = 10
-CoinsSystemPrompt =
-    "You are a Llama king of a kingdom. " ..
-    "A user wants you to give them some coins. Below is their reason. " ..
-    "Give them coins based on the reason. " ..
-    "Important: Respond tersely with a number of coins out of 10."
-
-DefaultSystemPrompt =
-    "You are the Llama king. " ..
-    "The user wants coins. Below is their reason. " ..
-    "Important: Respond only with a few words and GRADE: <number> 0 to 5."
-InferenceAllowList = {
-  -- LlamaKing ProcessId
-  "",
-}
 
 function Init()
   Llama = require("llama")
@@ -25,30 +8,17 @@ function Init()
   Llama.load("/data/" .. ModelID)
 end
 
-function ProcessPetition(systemPrompt, userPrompt)
-  Llama.setPrompt(GeneratePrompt(systemPrompt, userPrompt))
+function ProcessRequest(userPrompt, tokenCount)
+  Llama.setPrompt(userPrompt)
   local response = ""
-  for i = 1, DefaultMaxResponse do
+  for i = 1, tokenCount do
     response = response .. Llama.next()
-    local match = string.match(response, "(%d+)")
-    if match then
-      return {
-        Grade = tostring(match),
-        Reason = response,
-      }
-    end
   end
-  -- Default to 2
-  return {
-    Grade = "1",
-    Reason = "Your plea confused me immensely. Take these coins as a consolation.",
-  }
+  return response
 end
 
-function GeneratePrompt(systemPrompt, userPrompt)
-  return "<|SYSTEM|>" ..
-      systemPrompt .. "<|USER|>" ..
-      userPrompt .. "<|ASSISTANT|>"
+function GeneratePrompt(userPrompt)
+  return "<|USER|>" .. userPrompt .. "<|ASSISTANT|>"
 end
 
 Handlers.add(
@@ -57,11 +27,7 @@ Handlers.add(
   function(msg)
     ModelID = msg.Tags["Model-ID"] or ModelID
     Init()
-    DefaultSystemPrompt = msg.Tags.SystemPrompt or DefaultSystemPrompt
-    DefaultMaxResponse = msg.Tags["Max-Response"] or DefaultMaxResponse
-    Send({
-      Action = "Grader-Initialized",
-    })
+    Send({ Action = "Worker-Initialized" })
   end
 )
 
@@ -69,28 +35,20 @@ Handlers.add(
   "Inference",
   Handlers.utils.hasMatchingTag("Action", "Inference"),
   function(msg)
-    print("Inference")
+    print("Performing inference. Prompt: " .. msg.Data)
 
-    -- TODO: Whitelist
-    -- if not InferenceAllowList[msg.From] then
-    --   print("Inference not allowed: " .. msg.From)
-    --   return
-    -- end
+    if msg.From ~= RouterID then
+      print("Inference request from non-router. From: " .. msg.From)
+      return
+    end
 
-    local systemPrompt = msg.Tags.SystemPrompt or DefaultSystemPrompt
-    local userPrompt = msg.Data
-    local Grade, Reason = ProcessPetition(systemPrompt, userPrompt)
-    print("Grade: " .. Grade .. ", Reason: " .. Reason)
+    local response = ProcessRequest(msg.Data, tonumber(msg.Tokens))
+    print("Response:" .. response)
 
     ao.send({
       Target = msg.From,
-      Tags = {
-        Action = "Inference-Response",
-        Grade = Grade,
-        ["Original-Sender"] = msg.Tags["Original-Sender"],
-        ["Original-Message"] = msg.Tags["Original-Message"],
-      },
-      Data = Reason,
+      Action = "Inference-Response",
+      Data = response
     })
   end
 )

c8bf4e312092eae621a4cd8a0fc70f7436cefe9a
Sam Williams <sam@arweave.org>
2024-06-23 01:10:16 -0400

    Upgrade monitoring in the herder.

--- src/herder.lua
+++ src/herder.lua
@@ -27,13 +27,25 @@ FeePerToken = FeePerToken or 1
 
 LastMultiplier = LastMultiplier or 1
 
-function CalculateFee(prompt, tokens)
-    local tokenCount = tokens
-    for _ in string.gmatch(prompt, "%w+") do
-       tokenCount = tokenCount + 1
-    end
+Colors = {
+    red = "\27[31m",
+    green = "\27[32m",
+    blue = "\27[34m",
+    reset = "\27[0m",
+    gray = "\27[90m"
+}
 
-    return SetPromptFee + (tokenCount * FeePerToken)
+function CountWords(prompt)
+    local count = 0
+    for _ in string.gmatch(prompt, "%w+") do
+        count = count + 1
+    end
+    return count
+end
+
+-- Note: The herder counts words, not directs as counted by the LLM.
+function CalculateFee(prompt, tokens)
+    return SetPromptFee + ((CountWords(prompt) + tokens) * FeePerToken)
 end
 
 function DispatchWork()
@@ -43,19 +55,30 @@ function DispatchWork()
         end
 
         local work = table.remove(Queue, 1)
-        print("Dispatching work from queue. Priority: " .. work.multiplier .. ". Worker: " .. Herd[i])
+        print("[" .. Colors.gray .. "DISPATCHING WORK" .. Colors.reset .. " ]" ..
+            " Requested: " .. Colors.blue .. work.reqLength .. Colors.reset ..
+            " | Fee: " .. Colors.red .. string.sub(tostring(work.multiplier), 1, 4) .. "x" .. Colors.reset ..
+            " | In queue: " .. Colors.blue .. #Queue .. Colors.reset ..
+            " | Client: " .. Colors.blue .. string.sub(work.client, 1, 6) .. Colors.reset ..
+            " | Worker: " .. Colors.green .. string.sub(Herd[i], 1, 6) .. Colors.reset
+        )
+
         ao.send({
             Target = Herd[i],
             Action = "Inference",
             Tokens = work.tokens,
             Data = work.prompt,
+            Termination = work.termination
         })
 
         LastMultiplier = work.multiplier
 
         Busy[Herd[i]] = {
+            timestamp = work.timestamp,
             client = work.client,
-            userReference = work.userReference
+            userReference = work.userReference,
+            tokens = work.tokens,
+            reqLength = work.reqLength
         }
         table.remove(Herd, i)
     end
@@ -70,8 +93,6 @@ Handlers.add(
     function(msg)
         local needed = CalculateFee(msg["X-Prompt"], tonumber(msg["X-Tokens"]))
         local multiplier = tonumber(msg.Quantity) / needed
-
-        print("Inference request. Base fee: " .. needed .. " | Multiplier: " .. multiplier)
 
         if multiplier < 1 then
             print("Insufficient payment: " .. msg.Quantity)
@@ -98,16 +119,27 @@ Handlers.add(
             position = #Queue + 1
         end
 
+        local reqLength = tonumber(msg["X-Tokens"]) + CountWords(msg["X-Prompt"])
+
         table.insert(Queue, position, {
+            timestamp = msg.Timestamp,
             client = msg.Sender,
             prompt = msg["X-Prompt"],
             tokens = msg["X-Tokens"],
             multiplier = multiplier,
             transferID = msg.Id,
-            userReference = msg["X-Reference"]
+            userReference = msg["X-Reference"],
+            termination = msg["X-Termination"],
+            reqLength = reqLength
         })
 
-        print("Added to queue. Current size: " .. #Queue)
+        print("[" .. Colors.gray .. tostring(msg.Timestamp) .. Colors.reset .. ":" .. Colors.blue .. "REQ" .. Colors.reset .. "]" ..
+            " Requested: " .. Colors.blue .. reqLength .. Colors.reset ..
+            " | Fee: " .. Colors.red .. string.sub(tostring(multiplier), 1, 4) .. "x" .. Colors.reset ..
+            " | In queue: " .. Colors.blue .. #Queue .. Colors.reset ..
+            " | Client: " .. Colors.blue .. string.sub(msg.Sender, 1, 6) .. Colors.reset ..
+            " | Client ref: " .. Colors.gray .. string.sub(msg["X-Reference"], 1, 10) .. Colors.reset
+        )
 
         DispatchWork()
     end
@@ -117,17 +149,29 @@ Handlers.add(
     "InferenceResponseHandler",
     Handlers.utils.hasMatchingTag("Action", "Inference-Response"),
     function(msg)
-        print("Inference-Response. From: " .. msg.From .. ". Data length: " .. string.len(msg.Data))
         if not Busy[msg.From] then
-            print("Inference-Response not from worker.")
+            print("[" .. Colors.gray .. tostring(msg.Timestamp) .. Colors.reset .. ":" .. Colors.red .. "ERR" .. Colors.reset .. "]" ..
+                " Inference-Response not from worker." ..
+                " | Worker: " .. Colors.blue .. string.sub(msg.From, 1, 6) .. Colors.reset
+            )
             return
         end
+        
+        print("[" .. Colors.gray .. tostring(msg.Timestamp) .. Colors.reset .. ":" .. Colors.green .. "RES" .. Colors.reset .. "]" ..
+            " Requested: " .. Colors.green .. Busy[msg.From].reqLength .. Colors.reset ..
+            " | Duration: " .. Colors.blue .. string.sub(tostring((msg.Timestamp - Busy[msg.From].timestamp) / 1000), 1, 4) .. Colors.reset .. "s" ..
+            " (" .. Colors.red .. string.sub(tostring(Busy[msg.From].reqLength / ((msg.Timestamp - Busy[msg.From].timestamp) / 1000)), 1, 4) .. Colors.reset .. " tks/s)" ..
+            " | Client: " .. Colors.blue .. string.sub(Busy[msg.From].client, 1, 6) .. Colors.reset ..
+            " | Worker: " .. Colors.green .. string.sub(msg.From, 1, 6) .. Colors.reset ..
+            " | Client ref: " .. Colors.gray .. string.sub(Busy[msg.From].userReference, 1, 10) .. Colors.reset
+        )
 
         ao.send({
             Target = Busy[msg.From].client,
             Action = "Inference-Response",
             ["X-Reference"] = Busy[msg.From].userReference,
-            Data = msg.Data,
+            Data = msg.Data
+            -- Duration = msg.Timestamp - Busy[msg.From].timestamp
         })
 
         Busy[msg.From] = nil
@@ -141,7 +185,9 @@ Handlers.add(
     "Info",
     Handlers.utils.hasMatchingTag("Action", "Info"),
     function(msg)
-        print("Info request. From: " .. msg.From)
+        print("[" .. Colors.gray .. tostring(msg.Timestamp) .. Colors.reset .. ":" .. Colors.blue .. "INFO" .. Colors.reset .. "]" ..
+            " Client: " .. Colors.blue .. string.sub(msg.From, 1, 6) .. Colors.reset
+        )
         ao.send({
             Target = msg.From,
             Action = "Info-Response",
